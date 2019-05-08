8f8c315d6fc26ffe4f4d10db96856ed290c641fd
Johnathon Wood <jaywood128@gmail.com>
2019-05-07 21:05:28 -0400

    Push

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
                 u.name = auth['info']['name']
                 u.email = auth['info']['email']
                 u.image = auth['info']['image']
-                u.password = auth['info']['passsword']
+                u.password = auth['info']['credentials']
 
             end
             binding.pry
