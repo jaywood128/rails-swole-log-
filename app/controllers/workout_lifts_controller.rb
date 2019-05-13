9e57a7322a8e9b322a2c6f5bc21794589d833871
Johnathon Wood <jaywood128@gmail.com>
2019-05-13 16:14:56 -0400

    Changed current_user.workout_lifts to @workout.workout_lifts on a workout's workoutlifts index page

--- app/controllers/workout_lifts_controller.rb
+++ app/controllers/workout_lifts_controller.rb
@@ -1,58 +1,56 @@
 class WorkoutLiftsController < ApplicationController
-    def new
-        @workout_lift = WorkoutLift.new
-    end 
 
-    def create
-       
-        @workout = Workout.find_or_create_by(params[:workout_id]) 
-        
-        if @workout.valid? 
-            @workout_lift = current_user.workout_lifts.create(params_workout_lifts)
-        
-            redirect_to workout_workout_lifts_path(@workout)
-        else 
-            render :edit
-        end 
-    end 
+  def new
+    @workout_lift = WorkoutLift.new
+  end 
 
-    def show 
-      
-        @workout_lift = WorkoutLift.find(params[:id])
-    end 
+  def create
 
-    def edit
-        @workout_lift = WorkoutLift.find(params[:id])
-        @workout = Workout.find(params[:workout_id])
-    end 
-
-    def update 
+    @workout = Workout.find_or_create_by(params[:workout_id]) 
         
-      @workout = Workout.find(params[:workout_lift][:workout_id])
-      @workout_lift = WorkoutLift.find(params[:id])
+    if @workout.valid? 
+      @workout_lift = current_user.workout_lifts.create(params_workout_lifts)
         
-      @workout_lift.update(params_workout_lifts) 
-    
       redirect_to workout_workout_lifts_path(@workout)
-       
+    else 
+      render :edit
     end 
+  end 
 
-    def index 
-        @workout_lift = WorkoutLift.new
-        @workout_lift.exercise_sets.build
-        @workout_lift.exercise_sets.build
-        @workout = Workout.find(params[:workout_id])
-        @workout_lifts = @workout.workout_lifts
-    end 
+  def show  
+    @workout_lift = WorkoutLift.find(params[:id])
+  end 
 
-    def destroy
-        @workout_lift = WorkoutLift.find(params[:id])
-        @workout = Workout.find(params[:workout_id]) 
-        @workout_lift.destroy 
-        redirect_to workout_workout_lifts_path(@workout)
-    end 
+  def edit
+    @workout_lift = WorkoutLift.find(params[:id])
+    @workout = Workout.find(params[:workout_id])
+  end 
 
-    def params_workout_lifts 
-        params.require(:workout_lift).permit(:user_id, :lift_id, :lift, :workout_id, exercise_sets_attributes: [:id, :weight, :reps])
-    end 
+  def update    
+    @workout = Workout.find(params[:workout_lift][:workout_id])
+    @workout_lift = WorkoutLift.find(params[:id])
+        
+    @workout_lift.update(params_workout_lifts) 
+    
+    redirect_to workout_workout_lifts_path(@workout)   
+  end 
+
+  def index 
+    @workout_lift = WorkoutLift.new
+    @workout_lift.exercise_sets.build
+    @workout_lift.exercise_sets.build
+    @workout = Workout.find(params[:workout_id])
+    @workout_lifts = @workout.workout_lifts
+  end 
+
+  def destroy
+    @workout_lift = WorkoutLift.find(params[:id])
+    @workout = Workout.find(params[:workout_id]) 
+    @workout_lift.destroy 
+    redirect_to workout_workout_lifts_path(@workout)
+  end 
+
+  def params_workout_lifts 
+    params.require(:workout_lift).permit(:user_id, :lift_id, :lift, :workout_id, exercise_sets_attributes: [:id, :weight, :reps])
+  end 
 end
