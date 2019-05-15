ab51533108b311c87c284216bb39a9547b5c96e7
Johnathon Wood <jaywood128@gmail.com>
2019-05-15 17:16:41 -0400

    Working on User model scope method high_reps - no implicit conversion of a symbol into a string

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
     validates :name, presence: true 
     validates :email, presence: true, uniqueness: true 
     has_secure_password
+
+    scope :high_reps,  joins(:workout_lifts).join(:exercise_sets).where("exercise_sets.reps >= ?", 8 ) 
 end
 
 
